3caf871f123ed747696ff5751a5f11948e54e8b9
Paul R. Y. Taylor <paulrytaylor@gmail.com>
2025-04-01 21:07:47 -0400

    TICK_RATE updated from 20 to 50

--- Constants.java
+++ Constants.java
@@ -9,7 +9,7 @@ package JavaGameEngine;
  */
 public interface Constants
 {
-    int TICK_RATE=20; //the amount of ticks per second
+    int TICK_RATE=50; //the amount of ticks per second
     int DEFAULT_PANEL_WIDTH=600;
     int DEFAULT_PANEL_HEIGHT=400;
     double GRAVITY=1400; //pixels per second
